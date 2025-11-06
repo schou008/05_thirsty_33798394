9451d7ae1724bcd00dab9c2aaa683791aa67172d
schou008​ <saqibchy9@gmail.com>
2025-11-06 15:24:56 +0000

    Added comments to the code so it is more clear and understandable

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Setup express and ejs 
+//Setup express and ejs 
 var express = require ('express') 
 var ejs = require('ejs') 
 
-// Create the express application object 
+//Create the express application object 
 const app = express() 
 const port = 8000 
 
-// Serve files in the "cssFiles" folder
+//Serve files in the "cssFiles" folder
 app.use(express.static("cssFiles"));
 
-// Tell Express that we want to use EJS as the templating engine 
+//Tell Express that we want to use EJS as the templating engine 
 app.set('view engine', 'ejs'); 
 
-// Set up the body parser  
+//Set up the body parser  
 app.use(express.urlencoded({ extended: true }));  
 
-// Load the route handlers 
+//Load the route handlers 
 const mainRoutes = require("./routes/main"); 
 app.use('/', mainRoutes); 
 
-// Start the web app listening 
+//Start the web app listening 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,13 @@
-// Create a new router 
+//Create a new router 
 const express = require("express"); 
 const router = express.Router(); 
 
-// Define the Shops Data 
+//Define the Shops Data 
 var shopData = {
   shopName: "Simply Drinks",
   productCategories: ["Beer", "Wine", "Soft Drinks", "Hot Drinks"],
 
-  // Defines a list of shops with manager + address
+//Defines a list of shops with managers and addresses
   shops: [
     { manager: "Thomas Johnson", address: "12 High Street, London" },
     { manager: "Ben Smith", address: "22 Baker Street, London" },
@@ -17,29 +17,32 @@ var shopData = {
   ]
 };
 
-
-
-// Handle the main routes  
+//Handle the main routes
 router.get("/", function (req, res) { 
     res.render("index.ejs", shopData) 
 });     
 
+//Handles the about page
 router.get("/about", function (req, res) { 
     res.render("about.ejs", shopData) 
 });
 
+//Handles the search page
 router.get("/search", function (req, res) { 
     res.render("search.ejs", shopData); 
 });
 
+//Handles the search results
 router.get('/search_result', function (req, res) {  
     res.send("You searched for " + req.query.search_text + " in " + req.query.category); 
 });
 
+//Handles the register page
 router.get("/register", (req,res) => { 
     res.render("register.ejs",  shopData);  
 });  
 
+//Handles the user being registered
 router.post("/registered", (req, res) => {  
   res.send(
     'Hello ' + req.body.first + ' ' + req.body.last + 
@@ -47,7 +50,7 @@ router.post("/registered", (req, res) => {
   );   
 }); 
 
-// Show the survey form
+//Handles the survey form
 router.get('/survey', (req, res) => {
     res.render('survey', { 
         shopName: shopData.shopName,
@@ -55,7 +58,7 @@ router.get('/survey', (req, res) => {
     });
 });
 
-// Handle survey submission
+//Handles survey results
 router.post('/survey', (req, res) => {
     const response = req.body;
     res.render('surveyResult', { 
@@ -64,5 +67,5 @@ router.post('/survey', (req, res) => {
     });
 });
 
-// Export the router object so index.js can access it 
+//Export the router object so index.js can access it 
 module.exports = router; 
